68d55f5ea4f4fbdb0448a100b0449634f9c33b18
avrahamm <papaabramg@gmail.com>
2021-05-19 18:28:20 +0300

    from App down to TrRow

--- src/App.js
+++ src/App.js
@@ -1,30 +1,11 @@
 import React from "react";
-import {useSelector, useDispatch } from "react-redux";
+import {useSelector} from "react-redux";
 
 import HeaderRow from "./components/HeaderRow";
 import TrRow from "./components/TrRow";
-import * as actions from "./redux/actions";
 
 function App() {
     const ids = useSelector( state => state.rows.ids);
-    const dispatch = useDispatch();
-
-    const allowDrop = (ev) => {
-        console.log(`allowDrop ${ev.target.id}`);
-        ev.preventDefault();
-    }
-
-    const drag = (ev, draggedFromId) => {
-        console.log(`drag ${ev.target.id}, draggedFromId = ${draggedFromId}`);
-        ev.dataTransfer.setData("draggedFromId", draggedFromId);
-    }
-
-    const drop = (ev, droppedToRowId) => {
-        ev.preventDefault();
-        let draggedFromRowId = parseInt(ev.dataTransfer.getData("draggedFromId"));
-        console.log(`drop: draggedFromId = ${draggedFromRowId}, drop droppedToId = ${droppedToRowId}`);
-        dispatch(actions.dndRow(draggedFromRowId,droppedToRowId))
-    }
 
     return (
         <div className="App">
@@ -32,12 +13,7 @@ function App() {
             <HeaderRow id="header"/>
 
             {
-                ids.map( (id, key) => <TrRow key={key}
-                     id={id}
-                     allowDrop={allowDrop}
-                     drag={drag}
-                     drop={drop}
-                /> )
+                ids.map( (id, key) => <TrRow key={key} id={id} /> )
             }
 
         </div>
--- src/components/TrRow.js
+++ src/components/TrRow.js
@@ -1,23 +1,45 @@
 import React from "react";
+import {useDispatch} from "react-redux";
 
-function TrRow(props) {
+import * as actions from "../redux/actions";
+
+function TrRow({id}) {
+
+  const dispatch = useDispatch();
+
+  const allowDrop = (ev) => {
+    console.log(`allowDrop ${ev.target.id}`);
+    ev.preventDefault();
+  }
+
+  const drag = (ev, draggedFromId) => {
+    console.log(`drag ${ev.target.id}, draggedFromId = ${draggedFromId}`);
+    ev.dataTransfer.setData("draggedFromId", draggedFromId);
+  }
+
+  const drop = (ev, droppedToRowId) => {
+    ev.preventDefault();
+    let draggedFromRowId = parseInt(ev.dataTransfer.getData("draggedFromId"));
+    console.log(`drop: draggedFromId = ${draggedFromRowId}, drop droppedToId = ${droppedToRowId}`);
+    dispatch(actions.dndRow(draggedFromRowId,droppedToRowId))
+  }
 
   return (
-      <div id={`div-${props.id}`} className="table-responsive-sm"
-           onDrop={(ev) => props.drop(ev,parseInt(props.id))}
-           onDragOver={props.allowDrop}
+      <div id={`div-${id}`} className="table-responsive-sm"
+           onDrop={(ev) => drop(ev,parseInt(id))}
+           onDragOver={allowDrop}
       >
         <table className="table"
-               id={`drag-${props.id}`}
+               id={`drag-${id}`}
                draggable="true"
-               onDragStart={(ev) =>props.drag(ev,parseInt(props.id))}
+               onDragStart={(ev) =>drag(ev,parseInt(id))}
         >
           <tbody>
           <tr>
-            <th scope="row">{props.id}</th>
-            <td>First name {props.id}</td>
-            <td>Last name {props.id}</td>
-            <td>email @{props.id}</td>
+            <th scope="row">{id}</th>
+            <td>First name {id}</td>
+            <td>Last name {id}</td>
+            <td>email{id}@a.com</td>
           </tr>
           </tbody>
         </table>
